95600cf20e2c7834b8afc0bdce06367198e88b93
Lolom <lorenzoaoulini@gmail.com>
2023-08-10 10:20:12 +0200

    playwright test

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,6 @@ const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
 		url: 'http://localhost:4173',
-		port: 4173,
 		timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
 	},
